62e96a76fd52dcd43f2d4ccaae44781f0dfbd278
Hnsek <henrique2001sousa@gmail.com>
2025-09-09 14:46:06 -0300

    feat(templates): implements send request

--- email_analyser/urls.py
+++ email_analyser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('form/', views.form,name="index")
+    path('form/', views.form,name="form"),
+    path('send/', views.send,name="send")
 ]
