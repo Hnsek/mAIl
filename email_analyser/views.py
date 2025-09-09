807a0aa320bfd9b79e55c55e65bf39218fae4e29
Hnsek <henrique2001sousa@gmail.com>
2025-09-09 15:42:30 -0300

    feat(views): implements AI service

--- email_analyser/views.py
+++ email_analyser/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
+import requests
+from services import AI
 
 # Create your views here.
 def form(request):
@@ -10,15 +12,22 @@ def send(request):
     text = ''
     uploaded = request.FILES.get('file')
 
-    if not text and not uploaded:
+    if not request.POST.get('text') and not uploaded:
         return HttpResponseBadRequest('Data not send')
 
     
     if uploaded:
-        for chunk in uploaded.chunks():
-            text += chunk.decode('cp1252', errors='ignore')
+        raw = uploaded.read()
+        try:
+            text = raw.decode('utf-8')
+        except UnicodeDecodeError:
+            text = raw.decode('latin-1', errors='ignore')
     else:
         text = request.POST.get('text')
 
-    return HttpResponse(text)
+    prompt = 'Considerando um ambiente de trabalho, classifique de forma concisa o seguinte texto em: produtivo ou improdutivo. /n ' + text
+    
+    response = AI.get_response(prompt)
+    
+    return HttpResponse(response)
 
