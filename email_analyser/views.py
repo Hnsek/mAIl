c219518bb109146ae60240f60862a700f9a2fe11
Hnsek <henrique2001sousa@gmail.com>
2025-09-09 14:45:49 -0300

    feat(views): creates send view

--- email_analyser/views.py
+++ email_analyser/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
 def form(request):
     return render(request,"form.html")
+    
+def send(request):
+
+    text = ''
+    uploaded = request.FILES.get('file')
+
+    if not text and not uploaded:
+        return HttpResponseBadRequest('Data not send')
+
+    
+    if uploaded:
+        for chunk in uploaded.chunks():
+            text += chunk.decode('cp1252', errors='ignore')
+    else:
+        text = request.POST.get('text')
+
+    return HttpResponse(text)
 
