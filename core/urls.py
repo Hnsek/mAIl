0644218b128abdb0426f6c2090a2143a2c2e04c6
Hnsek <henrique2001sousa@gmail.com>
2025-09-09 16:21:15 -0300

    chore(core): changes app url to analyser

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('email-analyser/', include('analyser.urls'))
+    path('analyser/', include('analyser.urls'))
 ]
