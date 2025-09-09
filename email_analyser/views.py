9e848ab67e62bb5aec69c61ba6747dc2c6af3cc3
Hnsek <henrique2001sousa@gmail.com>
2025-09-09 16:18:54 -0300

    feat(view): implements files restrictions

--- email_analyser/views.py
+++ email_analyser/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest
-import requests
+from django.http import HttpResponseBadRequest
 from services import AI
+from services import files
 
 # Create your views here.
 def form(request):
@@ -16,21 +16,24 @@ def send(request):
         return HttpResponseBadRequest('Data not send')
 
     
+    
     if uploaded:
-        raw = uploaded.read()
-        try:
-            text = raw.decode('utf-8')
-        except UnicodeDecodeError:
-            text = raw.decode('latin-1', errors='ignore')
+
+        if uploaded.content_type != 'text/plain' and uploaded.content_type != 'application/pdf': 
+            return HttpResponseBadRequest('Format not accepted')
+
+        if uploaded.content_type == 'application/pdf':
+            text = files.extract_text_from_pdf(uploaded)
+        if uploaded.content_type == 'text/plain':
+            text = files.extract_text_from_txt(uploaded)
+        
     else:
         text = request.POST.get('text')
 
     prompt = 'Considerando um ambiente de trabalho, classifique de forma concisa o seguinte texto em: produtivo ou improdutivo. /n ' + text
     
     result = AI.get_response(prompt)
-    
-    context = { 
-        "result": result
-        }
+    context = { "result": result }
+
     return render(request,'result.html', context)
 
